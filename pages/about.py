8329fddfb65d102917c96e4f0b2cdea8594f3317
Ardeshir Beheshti <63427542+Ardy-EDFRE@users.noreply.github.com>
2022-07-06 16:06:18 -0700

    Create about.py

---
+++ pages/about.py
@@ -0,0 +1,49 @@
+import streamlit as st
+
+# Title of the main page
+st.image("https://raw.githubusercontent.com/Ardy-EDFRE/resource_assessment_tools/main/images/edf_medium_logo.png", width=400)
+
+st.markdown("# 📱 Resource Assessment Tools 📉")
+
+st.markdown("**ONE STOP SOLUTION FOR ALL YOUR DATA NEEDS**")
+st.markdown("## Introduction")
+
+st.markdown("## Tool Description")
+st.markdown("A collection of tools that can be used by Wind &amp; Solar teams to analyze specific resource assessments.")
+
+st.markdown("## Features")
+
+st.markdown("## 📝 Module-Wise Description")
+
+st.markdown("The application also uses Streamlit for a multiclass page implementation which can be viewed in the `multipage.py` file. The UI of the application can be seen here. The application is divided into multiple modules, each of which have been described below.")
+
+st.image("https://raw.githubusercontent.com/Ardy-EDFRE/resource_assessment_tools/main/images/tool_homepage.JPG", width=600)
+
+st.markdown("📌 **IEC Terrain Assessment**")
+st.image("https://raw.githubusercontent.com/Ardy-EDFRE/resource_assessment_tools/main/images/iec_results.JPG", width=600)
+
+st.markdown("📌 **DNV Solar Energy Estimate**")
+st.image("https://raw.githubusercontent.com/Ardy-EDFRE/resource_assessment_tools/main/images/dnv_solar_results.JPG", width=600)
+
+st.markdown("## Technology Stack")
+
+st.markdown("1. Python")
+st.markdown("2. Streamlit")
+
+st.markdown("## Other Content")
+
+st.markdown("## 🤝 How to Contribute?")
+
+st.markdown("- Take a look at the Existing Issues or create your own Issues!")
+st.markdown("- Wait for the Issue to be assigned to you after which you can start working on it.")
+st.markdown("- Fork the Repo and create a Branch for any Issue that you are working upon.")
+st.markdown("- Create a Pull Request which will be promptly reviewed and suggestions would be added to improve it.")
+st.markdown("- Add Screenshots to help us know what this Script is all about.")
+
+st.markdown("# 👨‍💻 Contributors ✨")
+
+st.markdown("## Contact")
+
+st.markdown("any feedback or queries, please reach out to [ardeshir.beheshti@edf-re.com](ardeshir.beheshti@edf-re.com).")
+
+st.image("https://raw.githubusercontent.com/Ardy-EDFRE/resource_assessment_tools/main/images/edf_small_logo.png", width=50)
